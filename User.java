ecca1e0fd576f0dede0995d41148278aefd019e0
banks81 <129882803+banks81@users.noreply.github.com>
2023-04-30 17:42:10 -0400

    Create User.java

---
+++ User.java
@@ -0,0 +1,37 @@
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String email;
+    private String name;
+    private String password;
+
+    public User(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
